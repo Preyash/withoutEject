f091cfee21bacb6f78a4286b7d418b93ed2f8c8e
Preyash <preyash.hojiwala@gmail.com>
2019-11-28 22:19:01 +0530

    first phase

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function submitForm(e){
+  e.preventDefault()
+  console.log('hello')
+}
+
+function App() {
+  return (
+    <div className="App">
+      <form onSubmit={(e)=>submitForm(e)}>
+      <p>Frstname: <input type="text"/> </p>
+      <p>Lastname: <input type="text"/> </p>
+      <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
+
